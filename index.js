7dfdf2384d8afcecfdce49566f0596039dc399c9
Ibrahim Yakubu Ibrahim <111001064+Legenddee@users.noreply.github.com>
2023-10-01 23:59:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const form03 = document.getElementById('Form-three');
 const form04 = document.getElementById('Form-four');
 const modal_submit = document.getElementById('modal-success');
 const window_Close = document.getElementById('window-exit');
+const Section_title = document.getElementById('Title');
 let userTitle = document.getElementById('form01-title');
 
 
 let verify_phone;
-// form01.style.display = 'none';
+form01.style.display = 'none';
 form02.style.display = 'none';
 form03.style.display = 'none';
-form04.style.display = 'none';
+// form04.style.display = 'none';
 modal_submit.style.display = 'none';
 
 
@@ -51,7 +52,8 @@ form04.addEventListener('submit', function(event) {
     form03.style.display ='none';
     modal_submit.style.display = 'block';
     modal_submit.click();
+    Section_title.style.display = 'none';
 });
-window_Close.addEventListener('click', function(){
-    modal_submit.style.display = 'none';
-});
+// window_Close.addEventListener('click', function(){
+//     modal_submit.style.display = 'none';
+// });
