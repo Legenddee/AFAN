327278d905b31b421c74557554be8236ef89d483
Ibrahim Yakubu Ibrahim <111001064+Legenddee@users.noreply.github.com>
2023-10-01 19:02:05 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-// let form01 = document.getElementById('Form-two');
-// let form02 = document.getElementById('Form-two');
-// form02.style.display = 'none'
-
-// let verify_btn = document.getElementById('verify-btn');
-
-// function verifyPhoneNumber() {
-//     form01.style.display ='none';
-//     form02.style.display ='block';
-// }
-
-// verify_btn.addEventListener('click', verifyPhoneNumber);
-
 const form01 = document.getElementById('Form-one');
 const form02 = document.getElementById('Form-two');
+let userTitle = document.getElementById('form01-title');
+
+let verify_phone;
 form02.style.display = 'none';
 
 
 form01.addEventListener('submit', function(event) {
     // Prevent the default form submission behavior
     event.preventDefault();
-    // You can add your custom logic here
-    // For example, validation or sending data to a server
     this.style.display ='none';
     form02.style.display = 'block';
+    verify_phone = document.getElementById('phone-validation-input').value;
+    userTitle.textContent = verify_phone;
 });
