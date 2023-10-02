39732b8a958913ef860778af71dba6ff676fc72d
Ibrahim Yakubu Ibrahim <111001064+Legenddee@users.noreply.github.com>
2023-10-02 05:13:12 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,23 +6,54 @@ const modal_submit = document.getElementById('modal-success');
 const window_Close = document.getElementById('window-exit');
 const Section_title = document.getElementById('Title');
 let userTitle = document.getElementById('form01-title');
+let warning_alert = document.getElementById('warning-alert');
 
 
-let verify_phone;
+let nameUser = document.getElementById('Applicant-Name');
+let Phone_Number = document.getElementById('Phone-Number');
+let Farm_produce = document.getElementById('Farm-Produce');
+let Phone_Verify = document.getElementById('phone-validation-input');
+
+
 // form01.style.display = 'none';
 form02.style.display = 'none';
 form03.style.display = 'none';
 form04.style.display = 'none';
 modal_submit.style.display = 'none';
+warning_alert.style.display = 'none';
 
 
+let checkPhone;
 form01.addEventListener('submit', function(event) {
     // Prevent the default form submission behavior
     event.preventDefault();
-    this.style.display ='none';
-    form02.style.display = 'block';
-    verify_phone = document.getElementById('phone-validation-input').value;
-    userTitle.textContent = verify_phone;
+    checkPhone = Phone_Verify.value;
+
+    if(checkPhone === "09039294814" || checkPhone === "+2349039294814") {
+        this.style.display ='none';
+        form02.style.display = 'block';
+        userTitle.textContent = checkPhone;
+
+        nameUser.value ="Ibrahim Yakubu Ibrahim";
+        Phone_Number.value = checkPhone;
+        Farm_produce.value = "RICE"
+    }
+    else if(checkPhone === "09013463576" || checkPhone === "+2349013463576"){
+        this.style.display ='none';
+        form02.style.display = 'block';
+        userTitle.textContent = checkPhone;
+
+        nameUser.value ="KENNETH OKECHUKWU";
+        Phone_Number.value = checkPhone;
+        Farm_produce.value = "MAIZE"
+
+    }
+    else{
+        this.style.display ='block';
+        warning_alert.style.display ='block';
+        Phone_Verify.style.color = "red";
+    }
+    
 });
 
 form02.addEventListener('submit', function(event) {
@@ -54,6 +85,4 @@ form04.addEventListener('submit', function(event) {
     modal_submit.click();
     Section_title.style.display = 'none';
 });
-// window_Close.addEventListener('click', function(){
-//     modal_submit.style.display = 'none';
-// });
+
